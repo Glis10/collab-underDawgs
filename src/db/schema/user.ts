830ad14627e923cc39741f3e3b3e46ffe22a8128
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-03-25 13:35:48 +0545

    feat(user.schema): add new fields + zod Schema

--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -8,9 +8,10 @@ import {
   bigint,
   timestamp,
   uuid,
+  json,
 } from "drizzle-orm/pg-core";
-import { createSelectSchema } from "drizzle-zod";
-import type { z } from "zod";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 import { emergencyRequest } from "./emergencyRequest";
 import { feedback } from "./feedback";
 import { locationTracking } from "./locationTracking";
@@ -27,8 +28,19 @@ export const user = pgTable("user", {
   password: varchar({ length: 255 }).notNull(),
   isVerfied: boolean("is_verified").default(false),
   role: userRolesEnum().default("user"),
+  profilePicture: varchar("profile_picture", { length: 255 }),
   verificationToken: varchar("verification_token", { length: 255 }),
   tokenExpiry: timestamp("token_expiry", { mode: "string" }),
+  socketId: varchar("socket_id", { length: 255 }),
+  currentLocation: json("current_location").default({
+    latitude: "",
+    longitude: "",
+  }),
+
+  resetPasswordToken: varchar("reset_password_token", { length: 255 }),
+  resetPasswordTokenExpiry: timestamp("reset_password_token_expiry", {
+    mode: "string",
+  }),
 
   createdAt: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
   updatedAt: timestamp("updated_at", { mode: "string" }).notNull().defaultNow(),
@@ -41,4 +53,21 @@ export const userRelations = relations(user, ({ many }) => ({
 }));
 
 export const usersSchema = createSelectSchema(user);
+export const newUserSchema = createInsertSchema(user).pick({
+  name: true,
+  age: true,
+  phoneNumber: true,
+  email: true,
+  primaryAddress: true,
+  password: true,
+});
+
+export const loginUserSchema = createInsertSchema(user).pick({
+  phoneNumber: true,
+  password: true,
+});
+export const userRolesSchema = createSelectSchema(userRolesEnum);
+
+export type TUserRole = z.infer<typeof userRolesSchema>;
 export type TUser = z.infer<typeof usersSchema>;
+export type TNewUser = z.infer<typeof newUserSchema>;
