904e9e9d56bc6c1c47f2e0f4605056f2358bfc4b
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-03-25 13:32:23 +0545

    refactor: removed handleBigInt as of now

--- src/utils/ApiResponse.ts
+++ src/utils/ApiResponse.ts
@@ -7,33 +7,33 @@ class ApiResponse {
   constructor(statusCode: number, message: string = "success", data: any) {
     this.statusCode = statusCode;
     this.message = message;
-    this.data = this.handleBigInt(data);
+    this.data = data;
     this.success = statusCode < 400;
   }
 
-  private handleBigInt(data: any): any {
-    if (data === null || data === undefined) return data;
+  // private handleBigInt(data: any): any {
+  //   if (data === null || data === undefined) return data;
 
-    if (typeof data === "bigint") {
-      return data.toString();
-    }
+  //   if (typeof data === "bigint") {
+  //     return data.toString();
+  //   }
 
-    if (Array.isArray(data)) {
-      return data.map(this.handleBigInt);
-    }
+  //   if (Array.isArray(data)) {
+  //     return data.map(this.handleBigInt);
+  //   }
 
-    if (typeof data === "object") {
-      const result: any = {};
-      for (const key in data) {
-        if (data.hasOwnProperty(key)) {
-          result[key] = this.handleBigInt(data[key]);
-        }
-      }
-      return result;
-    }
+  //   if (typeof data === "object") {
+  //     const result: any = {};
+  //     for (const key in data) {
+  //       if (data.hasOwnProperty(key)) {
+  //         result[key] = this.handleBigInt(data[key]);
+  //       }
+  //     }
+  //     return result;
+  //   }
 
-    return data;
-  }
+  //   return data;
+  // }
 }
 
 export default ApiResponse;
