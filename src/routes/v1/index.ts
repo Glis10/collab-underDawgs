8f2b8ee5d8a636b28da03f59a8baf88ea8323cbd
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-04-07 15:24:29 +0545

    feat(v1routes): add emergency-request routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 import userRouter from "@/routes/v1/user.routes";
 import serviceProviderRouter from "@/routes/v1/service-provider.routes";
 import organizationRouter from "@/routes/v1/organization.routes";
+import emergencyRequestRouter from "@/routes/v1/emergency-request.routes";
 
 const v1Router = Router();
 
 v1Router.use("/user", userRouter);
 v1Router.use("/service-provider", serviceProviderRouter);
 v1Router.use("/organization", organizationRouter);
+v1Router.use("/emergency-request", emergencyRequestRouter);
 
 export { v1Router };
