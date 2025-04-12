dfd93fda3d1f3bc55e212c48dd986fa6b2f227df
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-04-12 21:22:14 +0545

    feat: add socket event enums

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,12 @@
 export const getOtpMessage = (otpCode: string) => {
   return `Welcome to firstResQ. Your Login OTP Code is ${otpCode}`;
 };
+
+export enum SocketEventEnums {
+  CONNECTED_EVENT = "connected",
+  DISCONNECT_EVENT = "disconnect",
+  JOIN_EMERGENCY_ROOM = "joinEmergencyRoom",
+  LEAVE_EMERGENCY_ROOM = "leaveEmergencyRoom",
+  UPDATE_LOCATION = "updateLocation",
+  SOCKET_ERROR = "socketError",
+}
