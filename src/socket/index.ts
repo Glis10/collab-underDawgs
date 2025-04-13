ac40e4cc885ef133a19b2f6b4089ee37cccadf40
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-04-13 23:22:21 +0545

    feat: init socket stuffs

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -4,7 +4,6 @@ import { Server, Socket } from "socket.io";
 import { verifyJWT } from "@/utils/tokens/jwtTokens";
 import db from "@/db";
 import { eq } from "drizzle-orm";
-import { TNotification } from "@/db/schema/notification";
 import { SocketEventEnums } from "@/constants";
 import ApiError from "@/utils/api/ApiError";
 import type { Request } from "express";
@@ -14,12 +13,54 @@ type SocketUser = Socket & { user?: Partial<TUser> };
 const mountJoinRoomEvent = (socket: SocketUser) => {
   socket.on(
     SocketEventEnums.JOIN_EMERGENCY_ROOM,
-    ({ emergencyRequestId, userId, providerId }) => {
-      const room = `emergency_${emergencyRequestId}`;
+    ({ emergencyResponseId, userId, providerId }) => {
+      const room = `emergency:${emergencyResponseId}`;
       socket.join(room);
-      console.log(`${socket.id} joined ${room}`);
+      console.log(
+        `[SOCKET] ${socket.id} joined room emergency:${emergencyResponseId}`
+      );
     }
   );
+};
+
+const mountSendLocationEvent = (socket: SocketUser) => {
+  socket.on(
+    SocketEventEnums.SEND_LOCATION,
+    ({ emergencyResponseId, providerLocation }) => {
+      socket
+        .in(`emergency:${emergencyResponseId}`)
+        .emit(SocketEventEnums.UPDATE_LOCATION, {
+          userId: socket.user?.id,
+          location: providerLocation,
+        });
+      console.log(`[SOCKET] Location sent from ${socket.user?.id}`);
+    }
+  );
+};
+
+const mountHandleLocationUpdate = (socket: SocketUser) => {
+  socket.on(SocketEventEnums.UPDATE_LOCATION, async ({ location }) => {
+    if (!socket.user?.id) return;
+
+    const updateProvider = await db
+      .update(serviceProvider)
+      .set({
+        currentLocation: location,
+      })
+      .where(eq(serviceProvider.id, socket.user?.id))
+      .returning({
+        id: serviceProvider.id,
+        currentLocation: serviceProvider.currentLocation,
+      });
+
+    if (updateProvider.length === 0) {
+      throw new ApiError(500, "Error updating provider location");
+    }
+
+    console.log(
+      `[SOCKET] Location Update from ${socket.user?.id}, ${location}`
+    );
+  });
 };
 
 const authenticateUser = async (socket: SocketUser) => {
@@ -31,9 +72,17 @@ const authenticateUser = async (socket: SocketUser) => {
   const decoded = verifyJWT(token) as Partial<TUser>;
   if (!decoded || !decoded.id) throw new ApiError(401, "Unauthorized");
 
-  let loggedInEntity;
+  let loggedInEntity = null;
   loggedInEntity = await db.query.user.findFirst({
     where: eq(user.id, decoded.id),
+    columns: {
+      id: true,
+      name: true,
+      email: true,
+      phoneNumber: true,
+      role: true,
+      isVerified: true,
+    },
   });
 
   if (!loggedInEntity) {
@@ -44,6 +93,8 @@ const authenticateUser = async (socket: SocketUser) => {
         name: true,
         email: true,
         phoneNumber: true,
+        isVerified: true,
+        organizationId: true,
       },
     });
   }
@@ -51,26 +102,29 @@ const authenticateUser = async (socket: SocketUser) => {
   if (!loggedInEntity) throw new ApiError(401, "Unauthorized");
   socket.user = loggedInEntity;
   socket.join(loggedInEntity.id);
-  socket.emit(SocketEventEnums.CONNECTED_EVENT);
+  socket.emit(SocketEventEnums.CONNECTION_EVENT);
+  socket.emit(SocketEventEnums.AUTHORIZED_EVENT);
   console.log("User connected 🗼. userId: ", loggedInEntity.id.toString());
 };
 
-const initializeSocketIo = (io: Server) => {
-  return io.on(SocketEventEnums.CONNECTED_EVENT, handleSocketConnection);
-};
-
-const handleSocketConnection = (socket: SocketUser) => {
+const handleSocketConnection = async (socket: SocketUser) => {
   try {
-    authenticateUser(socket);
+    await authenticateUser(socket);
 
-    socket.on(SocketEventEnums.SOCKET_ERROR, (error: string) => {
-      console.log("user has disconnected 🚫. userId: " + socket.user?.id);
+    mountJoinRoomEvent(socket);
+    mountSendLocationEvent(socket);
+    mountHandleLocationUpdate(socket);
+
+    socket.on(SocketEventEnums.DISCONNECT_EVENT, (error: string) => {
+      console.log(
+        "[SOCKET]: User has disconnected 🚫. userId: " + socket.user?.id
+      );
       if (socket.user?.id) {
         socket.leave(socket.user.id);
       }
     });
   } catch (error: any) {
-    console.error("SOCKET: Error authenticating user", error);
+    console.error("[SOCKET]: Error authenticating user", error);
     socket.emit(
       SocketEventEnums.SOCKET_ERROR,
       error?.message || "Something went wrong while connecting to the socket."
@@ -78,7 +132,9 @@ const handleSocketConnection = (socket: SocketUser) => {
   }
 };
 
-const findCurrentLocation = async (providerId: string) => {};
+const initializeSocketIo = (io: Server) => {
+  return io.on(SocketEventEnums.CONNECTION_EVENT, handleSocketConnection);
+};
 
 const emitSocketEvent = (
   req: Request,
@@ -89,4 +145,4 @@ const emitSocketEvent = (
   req.app.get("io").in(roomId).emit(event, payload);
 };
 
-export { initializeSocketIo };
+export { initializeSocketIo, emitSocketEvent };
