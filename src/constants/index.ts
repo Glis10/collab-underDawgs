528aac63b013f267b813f5ea4c6f10b5c25924b9
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-04-13 23:18:47 +0545

    fix: typo of socket enum

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,10 +3,17 @@ export const getOtpMessage = (otpCode: string) => {
 };
 
 export enum SocketEventEnums {
-  CONNECTED_EVENT = "connected",
+  CONNECTION_EVENT = "connection",
   DISCONNECT_EVENT = "disconnect",
+
+  AUTHORIZED_EVENT = "authorized",
+
   JOIN_EMERGENCY_ROOM = "joinEmergencyRoom",
   LEAVE_EMERGENCY_ROOM = "leaveEmergencyRoom",
+  EMERGENCY_RESPONSE_CREATED = "emergencyResponseCreated",
+
   UPDATE_LOCATION = "updateLocation",
+  SEND_LOCATION = "sendLocation",
+
   SOCKET_ERROR = "socketError",
 }
