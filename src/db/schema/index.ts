7231fac610ba75ec29a0be1168da76ae2e626f29
rohit-Jung <157964658+rohit-Jung@users.noreply.github.com>
2025-04-15 23:36:26 +0545

    refactor: remove locationTracking export from schema

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,6 +1,5 @@
 export * from "./serviceProvider";
 export * from "./emergencyRequest";
-export * from "./locationTracking";
 export * from "./user";
 export * from "./emergencyResponse";
 export * from "./organization";
